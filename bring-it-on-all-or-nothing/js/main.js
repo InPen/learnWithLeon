9a9cdfb344636f2ed7120427a0b30382d887c674
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-10-01 13:56:45 -0400

    Add changes

--- bring-it-on-all-or-nothing/js/main.js
+++ bring-it-on-all-or-nothing/js/main.js
@@ -32,20 +32,22 @@ function dialog(boo, str){
 dialog(false,'sorry no kiss for you')
 //*Loops*
 //Create a function that takes in a number.
-function oldieGoodie(n1){
+function fizzBuzz(n1){
 //Console log all values from 1 to that number,
-  let n = 12
-  for(let i=1; i <= n; i++)
-  console.log(i)
+  // let n1 = 12
+  for(let i=1; i <= n1; i++) {
 //but if the number is divisible by 3 log "fizz" instead of that number,
-  if(n1 % 3) {
-    console.log('fizzz')
+    if(i %3===0 && i % 5===0) {
+      console.log('fizzzbuzzzz')
 //if the number is divisible by 5 log "buzz" instead of the number,
-  } else if(n % 5) {
-    console.log('buzzzzz')
+    } else if(i % 3===0) {
+        console.log('fizzz')
 //and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
-  } else if (n1 %3 && n1 % 5) {
-    console.log("fizzbuzz")
+    } else if (i % 5===0) {
+        console.log("buzz")
+    } else {
+      console.log(i)
+    }
   }
 }
-// oldieGoodie(6)
+fizzBuzz(666)
