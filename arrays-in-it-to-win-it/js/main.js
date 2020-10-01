7d23fe92b57d2d5ceba2630a75f34f8506b12546
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-10-01 14:21:42 -0400

    Include responses

--- arrays-in-it-to-win-it/js/main.js
+++ arrays-in-it-to-win-it/js/main.js
@@ -1,7 +1,12 @@
 //Create an array of movies with at least three movies.
-
+let movieArray = ['HP Deadly Hallows', 'The Ring', 'Dumpling', 'The Grudge']
+console.log(movieArray);
 //Using the array from above, store the first movie in a variable
-
+let hpSeries = movieArray[0]
+console.log(hpSeries);
 //Get the length of the original array and store it in a new variable
-
+arrayLenght = movieArray.length
+console.log(arrayLenght);
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+let lastElement = movieArray[3]
+console.log(lastElement);
