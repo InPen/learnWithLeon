a254caba06c5fd33ecfd212f56ae01f0358352f5
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-10-01 17:10:42 -0400

    Yay I made loopy progress and I'm playing with array mathods Kind of overthinking somethings but that's normal

--- arrays-fight-to-the-finish/js/main.js
+++ arrays-fight-to-the-finish/js/main.js
@@ -1,16 +1,30 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
-
-
+// let movies = ['Aaladin', 'Hercules', 'Us', 'HP and the Half Blood Prince', 'Princess Diaries']
+// movies.forEach(function(element){
+//   console.log(element);
+//   document.querySelector('h2').innerText += element
+// })
 //Create an array of numbers.
-let arrayOfNums = [88,666,4783,12] //[4,5,6,7,8]
+let arrayOfNums = [4,5,6,7,8] //[4,5,6,7,8]
 console.log(arrayOfNums.length);
 //Loop through the array and
- // for(let i=1;i<=arrayOfNums.length;i++){
+ for(let i=0;i< arrayOfNums.length;i++){
 //ADD three to each number and replace the old number
-//   arrayOfNums
-//   console.log(i)
-// }
-arrayOfNums.forEach(function(item, index) {
-  console.log((item + 3), index)
-})
+  arrayOfNums[i] = arrayOfNums[i]+3
+  console.log(arrayOfNums);
+}
+// arrayOfNums.forEach(function(item, index) {
+//   console.log((item + 3), index)
+//   console.log(arrayOfNums);
+// })
+//Johann's:
+// const newArrayOfNums = []
+// arrayOfNums.forEach(function(item, index) {
+//   newArrayOfNums.push(item+3)
+// })
+// console.log("");
+// console.log("");
+// console.log("arrayOfNums", arrayOfNums)
+// console.log("newArrayOfNums", newArrayOfNums)
+
 //Find the average of all the numbers from question three
