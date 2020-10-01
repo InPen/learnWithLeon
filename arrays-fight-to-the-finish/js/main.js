0cf358ae50566c621ad42fce5b823f509592580d
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-10-01 16:24:47 -0400

    Include work completed thus far

--- arrays-fight-to-the-finish/js/main.js
+++ arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,16 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
 
 
-//Create an array of numbers. Loop through the array and three to each number and replace the old number.
-
-
+//Create an array of numbers.
+let arrayOfNums = [88,666,4783,12] //[4,5,6,7,8]
+console.log(arrayOfNums.length);
+//Loop through the array and
+ // for(let i=1;i<=arrayOfNums.length;i++){
+//ADD three to each number and replace the old number
+//   arrayOfNums
+//   console.log(i)
+// }
+arrayOfNums.forEach(function(item, index) {
+  console.log((item + 3), index)
+})
 //Find the average of all the numbers from question three
