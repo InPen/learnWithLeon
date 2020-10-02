300066ca7a78d8400b7bb21d2fe3f717f345e922
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-10-02 17:16:20 -0400

    Include number to array response

--- arrays-worldwide-cheersmack/js/main.js
+++ arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,11 @@
-//Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+//Create a function that takes in a number.
+//The function should return an array that contains every number
+//from 1 to that number as seperate elements
+function numToArray(num){
+  let numArray = []
+  for(let i=1; i<num;i++){
+    numArray.push(i)
+  }
+  console.log(numArray)
+}
+numToArray(5)
