dbc03ca68246d0666b82bd89f773f3a18be7ce26
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-07 15:12:52 -0400

    Include console.log

--- review-winter-games/js/main.js
+++ review-winter-games/js/main.js
@@ -4,7 +4,7 @@ function numsArray(array){
 //Return a new array containing every even number from the original array (do not use map)
   for(let i = 0; i < array.length; i++) {
     if(array[i] % 2 === 0) {
-      evenNums.push(array[i])
+      console.log(evenNums.push(array[i]))
     }
   }
 }
