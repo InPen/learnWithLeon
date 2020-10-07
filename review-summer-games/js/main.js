ad712d1b87ffee59e05f2867a8c2b46d666991fa
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-07 14:45:28 -0400

    Include Response - that *= blew my mind

--- review-summer-games/js/main.js
+++ review-summer-games/js/main.js
@@ -1,1 +1,11 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers.
+
+function numArrayTaker(numArray){
+//Multiply each number together and alert the product.
+let product = 1
+for(let i = 0; i < numArray.length; i++){
+  product *= numArray[i]
+  }
+  console.log(product)
+}
+numArrayTaker([6,8,18,89,90,20]) 
