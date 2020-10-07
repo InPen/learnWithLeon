aa45eef7b8196fa166aa7480fd8255eb332d2200
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-07 12:20:05 -0400

    Include responses

--- review-in-paradise/js/main.js
+++ review-in-paradise/js/main.js
@@ -11,12 +11,17 @@ let index = 2
 console.log(`The second character in our string is ${wordVariable.charAt(index)}`)
 
 // *Functions*
-// Create a function that takes in 3 numbers. Divide the first two numbers and multiply the last. Alert the product. Call the function.
-
-
+// Create a function that takes in 3 numbers. Divide the first two numbers and multiply the last.
+//Alert the product. Call the function.
+function threeNums(n1,n2,n3){
+  console.log((n1/n2)*n3)
+}
+threeNums(909,18,6)
 // Create a function that takes in 1 number. Console log the cube root of the number. Call the function.
-
-
+function cubeRoot(number){
+  console.log(Math.cbrt(number))
+}
+cubeRoot(24)
 // *Conditionals*
 //Create a function that takes in a month. If it is a summer month alert "YAY". If another other month, alert "Booo"
 
