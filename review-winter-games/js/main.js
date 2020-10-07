9358be3095819bd6d0ffbe078604a41a4aa86f13
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-07 15:10:37 -0400

    Solve without using .map()

--- review-winter-games/js/main.js
+++ review-winter-games/js/main.js
@@ -1,1 +1,11 @@
-//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map)
+//Create a function that takes in an array of numbers.
+function numsArray(array){
+  let evenNums = []
+//Return a new array containing every even number from the original array (do not use map)
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] % 2 === 0) {
+      evenNums.push(array[i])
+    }
+  }
+}
+numsArray([32,81,6,8])
