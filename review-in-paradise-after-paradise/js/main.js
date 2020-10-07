dfb718c21e78352774263202d1c3459da12074f9
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-07 14:26:19 -0400

    Include final response

--- review-in-paradise-after-paradise/js/main.js
+++ review-in-paradise-after-paradise/js/main.js
@@ -1,14 +1,14 @@
 // Create a function that takes in an array.
-let array = [1,5,6,3,9,0]
 function arrayTaker(array){
 //If the first number, is less than the last number, alert "Hi".
-  if(array[0] < array.legth - 1){
+  if(array[0] < array[array.legth - 1]){
     console.log('Hewo')
 //If the first number is greater than the last number, alert "Bye".
-} else if(array[0] > array.legth - 1){
+} else if(array[0] > array[array.legth - 1]){
     console.log('Bye Felicia')
 //If they are equal, alert "We close in an hour".
-} else if(array[0] === array.legth - 1){
+} else {
     console.log("We close in an hour")
   }
 }
+arrayTaker([1,5,6,3,9,0])
