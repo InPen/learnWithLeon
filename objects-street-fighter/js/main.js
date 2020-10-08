828b5e30fd9f2a1b7c2b89542ab5c43da8799af3
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-08 13:11:49 -0400

    Include constructor function

--- objects-street-fighter/js/main.js
+++ objects-street-fighter/js/main.js
@@ -1,1 +1,18 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function StreetFighterConstructor(fighterName, fighterHairColor, fighterWeapon){
+  this.name = fighterName
+  this.hair = fighterHairColor
+  this.weapon = fighterWeapon
+  this.specialMove = function specialMove(){
+    console.log('specialMove')
+  }
+  this.kick = function kick(){
+    console.log('kick')
+  }
+  this.jump = function jump(){
+    console.log('jump')
+  }
+}
+
+const sailorMoon = new StreetFighterConstructor('Sailor Moon', 'Blonde', 'Moon Crystal')
+const stevenUniverse = new StreetFighterConstructor('Steven', 'Brown', 'Rose Quarts')
