5a98a9120bed605601ff4c80eb6fc95a840ecd85
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-14 11:54:36 -0400

    Add api URL and begin to target specific elements we want to display to user

--- objects-shots/js/main.js
+++ objects-shots/js/main.js
@@ -1,1 +1,15 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+//The user will enter a cocktail.
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+  .then(res => res.json()) //parse response as JSON
+  .then(data => {
+    // console.log(data.drinks[0])
+    document.querySelector('h2').src = data.drinks.strDrink
+    document.querySelector()
+    // strDrink = Name
+    // strDrinkThumb = img
+    // strInstructions = Instructions
+    // document.querySelector('img').src =data.message
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  })
