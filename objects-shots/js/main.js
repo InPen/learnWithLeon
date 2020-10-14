be9e246f1b9001270d3a0de4789773f283b9e3fb
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-14 12:02:32 -0400

    Finish adding the rest of the elements

--- objects-shots/js/main.js
+++ objects-shots/js/main.js
@@ -3,12 +3,12 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
   .then(res => res.json()) //parse response as JSON
   .then(data => {
     // console.log(data.drinks[0])
-    document.querySelector('h2').src = data.drinks.strDrink
-    document.querySelector()
+    document.querySelector('h2').innerText = data.drinks[0].strDrink
+    document.querySelector('img').src = data.drinks[0].strDrinkThumb
+    document.querySelector('h3').innerText = data.drinks[0].strInstructions
     // strDrink = Name
     // strDrinkThumb = img
     // strInstructions = Instructions
-    // document.querySelector('img').src =data.message
   })
   .catch(err => {
     console.log(`error ${err}`)
