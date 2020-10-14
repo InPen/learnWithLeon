8c40a5b79c2109c42cf1dca7a950547205286d5c
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-14 14:35:26 -0400

    Include event addEventListener for button Unable to display image based on date thus far

--- objects-nasa/js/main.js
+++ objects-nasa/js/main.js
@@ -1,16 +1,20 @@
 //The user will enter a date.
 //Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
-fetch("https://api.nasa.gov/planetary/apod?api_key=yuj47R6zXxhBHuABMScFfHPe6eIvuaStRMaelZX5")
-  .then(res => res.json()) //parse response as JSON
-  .then(data => {
-    console.log(data);
-    document.querySelector('h2').innerText = data.title
-    document.querySelector('img').src = data.hdurl
-    document.querySelector('h3').innerText = data.explanation
-    // "title"
-    // "hdurl"
-    // "explanation"
-  })
-  .catch(err => {
-    console.log(`error ${err}`)
-  })
+document.querySelector('button').addEventListener('click', () => {
+//!!I'm declaring date here and assigning it the html input but is no working
+  const date = document.querySelector('input').value
+  fetch("https://api.nasa.gov/planetary/apod?api_key=yuj47R6zXxhBHuABMScFfHPe6eIvuaStRMaelZX5")
+    .then(res => res.json()) //parse response as JSON
+    .then(data => {
+      console.log(data);
+      document.querySelector('h2').innerText = data.title
+      document.querySelector('img').src = data.hdurl
+      document.querySelector('h3').innerText = data.explanation
+      // "title"
+      // "hdurl"
+      // "explanation"
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    })
+})
