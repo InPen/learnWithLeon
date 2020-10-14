7d15a8ce5acb79f643f149c92ef73a8bd39066c9
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2020-10-14 14:21:11 -0400

    Include hard coded route

--- objects-nasa/js/main.js
+++ objects-nasa/js/main.js
@@ -1,1 +1,16 @@
-//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+//The user will enter a date.
+//Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+fetch("https://api.nasa.gov/planetary/apod?api_key=yuj47R6zXxhBHuABMScFfHPe6eIvuaStRMaelZX5")
+  .then(res => res.json()) //parse response as JSON
+  .then(data => {
+    console.log(data);
+    document.querySelector('h2').innerText = data.title
+    document.querySelector('img').src = data.hdurl
+    document.querySelector('h3').innerText = data.explanation
+    // "title"
+    // "hdurl"
+    // "explanation"
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  })
